dba3798ffdfdc1ac598ab9ca2d99f491dc17787b
dhew0016 <dhew0016@student.monash.edu>
2025-07-07 20:59:34 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv  from "dotenv";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -12,12 +13,16 @@ dotenv.config();
 // Initialize express app
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
+
+
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin:"http://localhost:5173", // Allow requests from this origin
+    })); // Enable CORS (Cross-Origin Reesource Sharing) for all routes
+};
 
 // Middleware to parse JSON requests
-app.use(cors({
-    origin:"http://localhost:5173", // Allow requests from this origin
-})); // Enable CORS (Cross-Origin Reesource Sharing) for all routes
-
 app.use(express.json()); // gives access to req.body in simple terms
 
 /**
@@ -31,6 +36,14 @@ app.use(rateLimiter); // Apply rate limiting custome middleware
 
 // Middleware to parse URL-encoded data
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+};
 
 // connect database first and start server
 connectDB().then(() => {
